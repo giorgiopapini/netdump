a65c8a9261032c364943920132fc867bf4a1aa58
giorgiopapini <giorgio.papini04@gmail.com>
2024-09-27 18:42:46 +0200

    Added first implementation of 'remove_spaces()' function

--- utils/string_utils.c
+++ utils/string_utils.c
@@ -19,9 +19,31 @@ void copy_str_n(char **dest, char *src, int n) {
     *dest = (char *)malloc(n + 1);
     memset(*dest, '\0', n + 1);
     strncpy(*dest, src, n);
+    remove_spaces(src);
     /* add trim functionality and change function name (from "copy_str_n" to something else) */
 }
 
-void trim(char *str) {
-
+void remove_spaces(char *str) {
+    char new_str[strlen(str) + 1];
+    size_t new_index = 0;
+    char *start = str;
+    char *end;
+    
+    while (*start != '\0') {
+        if (*start != ' ') {
+            new_str[new_index] = *start;
+            start ++;
+        }
+        else {
+            end = start + 1;
+            while (*end == ' ') end ++;
+            new_str[new_index] = ' ';
+            start = end;
+        }
+        new_index ++;
+    }
+    printf("new_index: %d,\n", new_index);
+    printf("len: %d", strlen(new_str));
+    printf("\nNEW_STR:%s\n", new_str);
+    /* check eventual unnecessary leading and trailing whitespaces */
 }
