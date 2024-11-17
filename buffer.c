29d8d3927dbe54ac3c127c876dc40b176cf4be03
giorgiopapini <giorgio.papini04@gmail.com>
2024-11-17 16:42:55 +0100

    Refactored 'buffer.c' module. Now 'canc()' and 'backspace()' functions are smaller and more readable

--- buffer.c
+++ buffer.c
@@ -14,10 +14,22 @@
 #define ARROW_LEFT_KEY 'D'
 #define CANC_KEY '3'
 
+
 void normalize_content(buffer *buff) {
     char *normalized_str = get_trimmed_str(buff->content);
     strncpy(buff->content, normalized_str, MAX_BUFFER_LEN - 1);
     buff->content[buff->len] = '\0';
+}
+
+void refresh_output(buffer *buff, int pos) {
+    int i;
+
+    if (pos != buff->len) printf("\033[%dC", buff->len - pos);  /* move the cursor to end of string if not alredy there */
+    for (i = buff->len; i > 0; i --) printf("\b \b");  /* delete old string */   
+    for (i = 0; i < buff->len; i ++) printf("%c", buff->content[i]);  /* print updated string (prints also final '\0') */;  
+
+    if ((buff->len - 1) != pos) printf("\033[%dD", (buff->len - 1) - pos);  /* reset the pointer to previous position */
+    /* refresh_output() should not change any state of the buffer. The length is then decremented in the caller function */
 }
 
 void arrow_up() {
@@ -36,7 +48,7 @@ void arrow_right(buffer *buff, int *pos) {
 }
 
 void arrow_left(buffer *buff, int *pos) {
-    if ((*pos) > 0) {
+    if (*pos > 0) {
         printf("\033[D");
         *pos = *pos - 1;
     }
@@ -48,40 +60,32 @@ void canc(buffer *buff, int *pos) {
     if (buff->len == *pos) return;
 
     delete_char(buff->content, *pos);
-    printf("\033[%dC", buff->len - *pos);  /* move the cursor to end of string */
-    for (i = buff->len; i > 0; i --) printf("\b \b");  /* delete old string */
-    for (i = 0; i < buff->len; i ++) printf("%c", buff->content[i]);  /* print updated string */;
-    
-    /* if len == 1 it would erase some chars of the prompt "netdump > ". This condition prevent this behaviour */
-    if (buff->len > 1) printf("\033[%dD", buff->len - *pos - 1);  /* move the cursor to original position */
-
-    /* if last char deleted and pos > 0 (not last first char), decrease position by one (pos-1=shift pointer to left) */
-    if (*pos == (buff->len - 1) && *pos > 0) *pos = *pos - 1;
+    refresh_output(buff, *pos);
     buff->len --;
 }
 
 void backspace(buffer *buff, int *pos) {
-    printf("\b \b");    /* erase previous printf output char */
-    buff->content[buff->len - 1] = '\0';
+    if (0 >= *pos) return;
+
+    delete_char(buff->content, *pos - 1);
+    refresh_output(buff, *pos);
+
+    if (*pos < buff->len) arrow_left(buff, pos);
+    else *pos = *pos - 1;
     buff->len --;
-    *pos = *pos - 1;
 }
 
-void literal_key(buffer *buff, int *pos, char c) {
+void literal_key(buffer *buff, int *pos, char c) {   /* add character writing in mid string (not only at the end) */
     buff->content[buff->len] = c;
     buff->len ++;
     *pos = *pos + 1;
 }
 
-/* 
-    IMPORTANT!!! --> Manage the deleting and writing of characters mid word. Mybe extract some code from the
-    canc() function to reuse the same logic. Write this behaviours inside backspace and literal_key functions
-*/
-
 void populate(buffer *buff) {
     char c = 0;
     int pos = 0;
-    buff->content[0] = '\0';
+
+    memset(buff->content, 0, buff->len);
     buff->len = 0;
     buff->status = 0;
 
