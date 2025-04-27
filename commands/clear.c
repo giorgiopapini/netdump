d5dd47767e425a15eef12cf753afeef0af7b7e3e
giorgiopapini <giorgio.papini04@gmail.com>
2025-04-27 19:47:27 +0200

    Solved clear command unrecognized on *BSDs systems

--- commands/clear.c
+++ commands/clear.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
+
 #include "clear.h"
+#include "./status_handler.h"
 
 void execute_clear(command *cmd) {
     #if defined(_WIN32) || defined(_WIN64)
         system("cls");
-    #elif defined(__linux__) || defined(__APPLE__)
+        #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
         system("clear");
     #else
         raise(COMMAND_NOT_SUPPORTED_ERROR, 0, CLEAN_COMMAND);
